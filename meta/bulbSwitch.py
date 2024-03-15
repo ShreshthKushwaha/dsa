02acc3a6165ab31c55a303ac3c9f06a90f78deeb
ShreshthKushwaha <shreshth.kushwaha@tamu.edu>
2024-03-14 21:06:57 -0500

    bulb switch added

---
+++ meta/bulbSwitch.py
@@ -0,0 +1,11 @@
+##suppose there are 1 to n bulbs and there is a bulb k
+#it would be toggled the number of times the multiples
+#eg : 16
+#it can be toggled, 1,2,4,8,16 times 
+#15..it can be toggled 1,3,5,15
+#2..it can be toggled 1,2 
+# in all the number of multiple times it can be toggled
+#if number of multiples are odd then they will be left on
+#logically only perfect squares have odd numbers of multiples
+#in all the number of perfect squares within a range is the answer
+#which is int(n**0.5)
